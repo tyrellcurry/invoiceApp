cc93bf9ef9f8858852a19464b8caae9e1785f52e
Tyrell C <tyrellrcurry@gmail.com>
2024-12-28 12:21:12 -0800

    Storybook setup

---
+++ frontend/src/stories/components/UI/atoms/Text.stories.tsx
@@ -0,0 +1,22 @@
+import type { StoryObj } from '@storybook/react';
+
+import Text from '@/app/components/UI/atoms/Text/Text';
+
+const meta = {
+  title: 'Atoms/Text',
+  component: Text,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'h1',
+    children: 'Hello, World!',
+  },
+};
