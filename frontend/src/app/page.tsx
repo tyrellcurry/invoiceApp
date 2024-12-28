89648eb670b7be3dce117086e134d460698f6ef0
Tyrell C <tyrellrcurry@gmail.com>
2024-12-27 17:27:48 -0800

    Tailwind color config and Google font import

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Invoice App</h1>
+      <h1 className="text-blue-200 text-4xl font-bold">Invoice App</h1>
     </div>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,16 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ['var(--font-leaguespartan)'],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      black: '#000000',
+      blue: {
+        100: '#DFE3FA', // Light Blue
+        200: '#7E88C3', // Muted Blue
+        300: '#7C5DFA', // Soft Purple-Blue
+      },
+      gray: {
+        100: '#F8F8FB', // Very Light Gray
+        200: '#DFE3FA', // Light Gray
+        300: '#888EB0', // Medium Gray
+        400: '#1E2139', // Dark Gray-Blue
+        500: '#252945', // Very Dark Gray-Blue
+        600: '#141625', // Very Dark Gray-Black
+        700: '#0C0E16', // Blackish Gray
+      },
+      red: {
+        400: '#9277FF', // Soft Light Ref
+        500: '#EC5757', // Red
       },
     },
   },
