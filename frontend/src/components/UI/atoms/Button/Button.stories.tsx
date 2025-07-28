6ab4e97ab6a5e68b0f93da3b0795d24aba944521
tyrellcurry <tyrellcurryio@gmail.com>
2025-07-27 21:02:08 -0700

    IA-16 :: Button component refactor

--- frontend/src/components/UI/atoms/Button/Button.stories.tsx
+++ frontend/src/components/UI/atoms/Button/Button.stories.tsx
@@ -14,36 +14,52 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: 'Hello, World!',
+    label: 'Hello, World!',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    children: 'Hello, World!',
+    label: 'Hello, World!',
     variant: 'secondary',
   },
 };
 
 export const Dark: Story = {
   args: {
-    children: 'Hello, Dark World!',
+    label: 'Hello, Dark World!',
     variant: 'dark',
   },
 };
 
 export const Danger: Story = {
   args: {
-    children: 'Hello, Delete World!',
+    label: 'Hello, Delete World!',
     variant: 'danger',
   },
 };
 
 export const ButtonWithIcon: Story = {
   args: {
-    children: 'Button w/ icon',
+    label: 'Button w/ icon',
     className: 'gap-1',
     iconRight: 'dm-moon',
     iconRightClassName: 'w-6 h-6 fill-blue-03',
+  },
+};
+
+export const ButtonAsLink: Story = {
+  args: {
+    label: 'Button As Link',
+    className: 'gap-1',
+    href: '#',
+  },
+};
+
+export const ButtonWithText: Story = {
+  args: {
+    className: 'gap-1',
+    href: '#',
+    label: 'test',
   },
 };
