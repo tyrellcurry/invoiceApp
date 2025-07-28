fac78e79fcfabd7c5a6ba5a936abb9984d410cc5
tyrellcurry <tyrellcurryio@gmail.com>
2025-07-27 21:22:16 -0700

    IA-16 :: Checkbox component refactor

--- frontend/src/components/UI/atoms/Input/Checkbox/Checkbox.tsx
+++ frontend/src/components/UI/atoms/Input/Checkbox/Checkbox.tsx
@@ -1,16 +1,28 @@
+/**
+ * @name Checkbox
+ * @author Tyrell Curry <tyrellcurryio@gmail.com>
+ *
+ * Used for rendering checkbox
+ *
+ * @param labelId
+ * @param label
+ *
+ * @returns {JSX.Element}
+ */
+
 import { ICheckboxProps } from '@/components/UI/atoms/Input/Checkbox/Checkbox.interface';
 import Text from '@/components/UI/atoms/Text/Text';
 import { JSX } from 'react';
 
 const Checkbox = (props: ICheckboxProps): JSX.Element => {
-  const { labelId, label, onChange } = props;
+  const { labelId, label, ...rest } = props;
   return (
     <div className="flex items-center gap-x-[13px]">
       <input
         className="relative peer shrink-0 appearance-none w-5 h-5 bg-gray-05 dark:bg-blue-03 border-2 border-blue-01 rounded-sm checked:bg-blue-01 dark:checked:bg-blue-01 checked:border-0 cursor-pointer"
         id={labelId}
         type="checkbox"
-        onChange={onChange}
+        {...rest}
       />
       <Text
         className="cursor-pointer font-bold select-none dark:text-white"
@@ -26,8 +38,8 @@ const Checkbox = (props: ICheckboxProps): JSX.Element => {
         width="10"
         xmlns="http://www.w3.org/2000/svg"
         className="
-      absolute 
-      w-3.5 h-3.5 ml-0.5 
+      absolute
+      w-3.5 h-3.5 ml-0.5
       hidden peer-checked:block pointer-events-none"
       >
         <path d="M1.5 4.49976L3.62425 6.62402L8.96995 1.27832" stroke="white" strokeWidth="2" />
